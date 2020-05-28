94a3f5a943a41b756ebc162c544d7bcd969e6ef8
FrostedDev <58970190+FrostedDev@users.noreply.github.com>
2020-05-28 13:23:18 -0400

    Update Resources.java

--- GitHub FadeClient/ca/fadeclient/utils/Resources.java
+++ GitHub FadeClient/ca/fadeclient/utils/Resources.java
@@ -2,13 +2,25 @@ package ca.fadeclient.utils;
 
 import net.minecraft.util.ResourceLocation;
 
-public class Resources {
+public enum Resources {
 
-    public static ResourceLocation logo = new ResourceLocation("fadeclient/gui/mainmenu/FadeClientLogo.png");
-    public static ResourceLocation logo2 = new ResourceLocation("fadeclient/gui/mainmenu/logo2.png");
-    public static ResourceLocation background = new ResourceLocation("fadeclient/gui/mainmenu/background.png");
-    public static ResourceLocation background2 = new ResourceLocation("fadeclient/gui/mainmenu/background2.png");
-    public static ResourceLocation exit = new ResourceLocation("fadeclient/gui/mainmenu/quit.png");
-    public static ResourceLocation options = new ResourceLocation("fadeclient/gui/mainmenu/options.png");
+    LOGO(new ResourceLocation("fadeclient/gui/mainmenu/FadeClientLogo.png")),
+    LOGO2(new ResourceLocation("fadeclient/gui/mainmenu/logo2.png")),
+    BACKGROUND(new ResourceLocation("fadeclient/gui/mainmenu/background.png")),
+    BACKGROUND2(new ResourceLocation("fadeclient/gui/mainmenu/background2.png")),
+    EXIT(new ResourceLocation("fadeclient/gui/mainmenu/quit.png")),
+    OPTIONS(new ResourceLocation("fadeclient/gui/mainmenu/options.png")),
+    ;
+
+    public ResourceLocation rl;
+
+    private Resources(ResourceLocation rl)
+    {
+        this.rl = rl;
+    }
+
+    public ResourceLocation getResourceLocation(){
+        return rl;
+    }
 
 }
